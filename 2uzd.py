6d43704b371b5468edc48f21be9bcdd89f69ff3d
livapliksa <liva.pliksa@grobinasskola.lv>
2023-11-16 14:47:12 +0200

    Pievienot 4uzd.py

--- 2uzd.py
+++ 2uzd.py
@@ -1,4 +1,4 @@
 num = 1
-while num ** 2 <= 1000:
+while num * 2 <= 1000:
     num += 1
 print("Pirmais skaitlis, kura kvadrāts ir lielāks par 1000, ir", num)
---
+++ 4uzd.py
@@ -0,0 +1,5 @@
+num = int(input("Ievadiet skaitli: "))
+faktoriāls = 1
+for i in range(1, num + 1):
+    faktoriāls = i
+print("Skaitļa faktoriāls ir", faktoriāls)
